df6853640bc600a4503047e2e6bb61bb6404d459
baqtdiyc <cfryoken@qq.com>
2018-09-21 17:30:11 +0800

    test v0.4

--- assets/firstgame/background.js
+++ assets/firstgame/background.js
@@ -3,13 +3,19 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        bg_map:[cc.Node],
-        seepdx:3,
+        bg_map: [cc.Node],
+        seepdx: 3,
+        audios: {
+            type:cc.AudioClip,
+            default:null
+        }
     },
 
-    // LIFE-CYCLE CALLBACKS:
 
-    // onLoad () {},
+    onLoad () {
+        var aa = cc.audioEngine.play(this.audios,true,1);
+    },
+
 
     start () {
 
@@ -18,6 +24,7 @@ cc.Class({
     update (dt) {
         this.bg_move(this.bg_map,this.seepdx);
     },
+
     bg_move:function(bg_list,seepd){
         for(var index = 0; index < bg_list.length; index++){
             bg_list[index].y -= seepd;
@@ -25,5 +32,5 @@ cc.Class({
                 bg_list[index].y = 960;
             }
         }
-    }
+    },
 });
